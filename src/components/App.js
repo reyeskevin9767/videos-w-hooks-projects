6b642153d4d7d7e460c63962a068862b9e301518
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-27 23:59:28 -0400

    reactor app

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import youtube, { baseParams } from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
   // Manually call onTermSumbit when application first loads
-  componentDidMount() {
-    this.onTermSubmit('react js');
-  }
+  useEffect(() => {
+    onTermSubmit('react js');
+  }, []);
 
   // Send a request to api
-  onTermSubmit = async (term) => {
+  const onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         ...baseParams,
@@ -21,39 +22,32 @@ class App extends React.Component {
       },
     });
 
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   };
 
   // Get info about video from videoItem
   // Communicate from parent to child with props
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
   };
 
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui stackable grid ">
-          <div className="row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <div className="ui stackable grid ">
+        <div className="row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
 
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
